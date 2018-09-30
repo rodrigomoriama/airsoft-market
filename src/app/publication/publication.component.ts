1d5c9dd687ea2561b550cff26acea98fe8db5177
rodrigomoriama <rodrigo@moriama.com.br>
2018-09-29 22:58:44 -0300

    atualização

--- src/app/publication/publication.component.ts
+++ src/app/publication/publication.component.ts
@@ -1,22 +1,47 @@
+import { FormatFieldHelper } from './../helpers/format-field-helper';
+import { debounceTime } from 'rxjs/internal/operators/debounceTime';
+import { Subscription } from 'rxjs/internal/Subscription';
+import { URLSearchParams } from '@angular/http';
+import { forkJoin } from 'rxjs';
+import { PublicationService } from './../../providers/publication.service';
+import { Dropdown } from './../../domain/dropdown';
 import { AppConstants } from './../app.constants';
 import { FormGroup, FormBuilder } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-publication',
   templateUrl: './publication.component.html',
   styleUrls: ['./publication.component.css']
 })
-export class PublicationComponent implements OnInit {
+export class PublicationComponent implements OnInit, AfterViewInit, OnDestroy {
 
   publicationForm: FormGroup;
 
   codeOperationType: number[];
 
-  constructor(private formBuilder: FormBuilder) {
+  productType: Dropdown[];
+  conditionType: Dropdown[];
+  caliberType: Dropdown[];
+  systemType: Dropdown[];
+  location: Dropdown[];
+  model: Dropdown[];
+  manufacturer: Dropdown[];
+  materialType: Dropdown[];
+
+  emptyItem: Dropdown;
+
+  subscriptionLocation: Subscription;
+  subscriptionModel: Subscription;
+  subscriptionManufacturer: Subscription;
+
+  constructor(private formBuilder: FormBuilder,
+    private publicationService: PublicationService) {
+
+    this.emptyItem = { id: null, name: '' };
+
     this.publicationForm = this.formBuilder.group({
       'id': [null],
-      'codeOperationType': [null],
       'codeProductType': [null],
       'codeModel': [null],
       'codeManufacturer': [null],
@@ -33,7 +58,7 @@ export class PublicationComponent implements OnInit {
       'hasUpgrade': [false],
       'acceptExchange': [false],
       'opBuy': [false],
-      'opSell': [false],
+      'opSell': [true],
       'opExchange': [false],
 
 
@@ -54,14 +79,82 @@ export class PublicationComponent implements OnInit {
 
       'images_path': ['', '', '', '', ''],
 
-      'newModel': [''],
-      'newManufacture': [''],
+      'locationName': [''],
+      'modelName': [''],
+      'manufacturerName': ['']
 
     });
   }
 
   ngOnInit() {
     this.codeOperationType = [];
+    this.loadFieldsData();
+  }
+
+  ngAfterViewInit() {
+    this.subscribeCombos();
+  }
+
+  ngOnDestroy() {
+    this.subscriptionLocation.unsubscribe();
+  }
+
+  onSubmit() {
+    const formData = new FormData;
+    formData.set('codeOperationType', <any>this.codeOperationType);
+
+    console.log(this.publicationForm.value);
+  }
+
+  loadFieldsData() {
+    forkJoin(
+      this.publicationService.getProductType(new URLSearchParams),
+      this.publicationService.getConditionType(new URLSearchParams),
+      this.publicationService.getCaliberType(new URLSearchParams),
+      this.publicationService.getSystemType(new URLSearchParams),
+      this.publicationService.getManufacturer(new URLSearchParams),
+      this.publicationService.getMaterialType(new URLSearchParams),
+    ).subscribe(
+      (data: [Dropdown[], Dropdown[], Dropdown[], Dropdown[], Dropdown[], Dropdown[]]) => {
+
+        this.productType = data[0];
+        this.conditionType = data[1];
+        this.caliberType = data[2];
+        this.systemType = data[3];
+        this.manufacturer = data[4];
+        this.materialType = data[5];
+
+        this.productType.unshift(this.emptyItem);
+        this.conditionType.unshift(this.emptyItem);
+        this.caliberType.unshift(this.emptyItem);
+        this.systemType.unshift(this.emptyItem);
+        this.materialType.unshift(this.emptyItem);
+      },
+      error => console.log(error)
+      );
+  }
+
+  subscribeCombos() {
+    this.subscriptionLocation = this.publicationForm.get('locationName').valueChanges
+      .pipe(
+      debounceTime(400)
+      ).subscribe(value => {
+        this.onFilterLocation(value);
+      });
+
+    this.subscriptionModel = this.publicationForm.get('modelName').valueChanges
+      .pipe(
+      debounceTime(400)
+      ).subscribe(value => {
+        this.onFilterModel(value);
+      });
+
+    this.subscriptionManufacturer = this.publicationForm.get('manufacturerName').valueChanges
+      .pipe(
+      debounceTime(400)
+      ).subscribe(value => {
+        this.onFilterManufacturer(value);
+      });
   }
 
   selectOperationType(value: any, operationType: number): void {
@@ -114,6 +207,174 @@ export class PublicationComponent implements OnInit {
     }
 
     console.log(this.codeOperationType);
+  }
+
+  canClearField(fieldName: string): boolean {
+    return this.publicationForm.get(fieldName).value && this.publicationForm.get(fieldName).enabled;
+  }
+
+  onSelectedProductType() {
+    // after load product type, load model
+    if (this.publicationForm.get('codeProductType').value) {
+      const params = new URLSearchParams();
+      params.set('productType', this.publicationForm.get('codeProductType').value);
+      this.onClearModelDropdown();
+      this.model = [];
+      this.loadModel(params);
+    }
+  }
+
+  // ===================================================
+  // LOCATION
+  // ===================================================
+  onSelectedItemLocation(event) {
+    if (event !== undefined || event !== null) {
+
+      if (event.id) {
+        this.selectLocation(event);
+      } else if (event !== '') {
+        const findItem = this.location.find(
+          l => FormatFieldHelper.removeAccents(l.name).toLowerCase() === FormatFieldHelper.removeAccents(event).toLowerCase());
+
+        if (findItem) {
+          this.selectLocation(findItem);
+          return;
+        }
+      } else {
+        this.onClearLocationDropdown();
+      }
+    }
+  }
+
+  selectLocation(event: Dropdown) {
+    console.log(event);
+    this.publicationForm.get('codeLocation').setValue(event.id);
+    this.publicationForm.get('locationName').setValue(event.name);
+  }
+
+  onClearLocationDropdown() {
+    this.publicationForm.get('codeLocation').setValue(null);
+    this.publicationForm.get('locationName').setValue('');
+  }
+
+  onFilterLocation(event: string) {
+    const params = new URLSearchParams();
+    params.set('name_like', event);
+
+    this.publicationService.getLocationCity(params).subscribe(
+      (location: Dropdown[]) => {
+        this.location = location;
+      },
+      error => console.log(error)
+    );
+  }
+
+  // ===================================================
+  // MODEL
+  // ===================================================
+  onSelectedItemModel(event) {
+    // only if user has selected product Type
+    if (this.publicationForm.get('codeProductType').value) {
+      if (event !== undefined || event !== null) {
+
+        if (event.id) {
+          this.selectModel(event);
+        } else if (event !== '') {
+          // NEW MODEL
+          const findItem = this.model.find(
+            p => FormatFieldHelper.removeAccents(p.name).toLowerCase() === FormatFieldHelper.removeAccents(event).toLowerCase());
+          if (findItem) {
+            this.selectModel(findItem);
+            return;
+          }
+
+          console.log('New Model: ' + event);
+          this.publicationForm.get('codeModel').setValue(null);
+          this.publicationForm.get('modelName').setValue(event);
+        } else {
+          this.onClearModelDropdown();
+        }
+      }
+    }
+  }
+
+  selectModel(event: Dropdown) {
+    console.log(event);
+    this.publicationForm.get('codeModel').setValue(event.id);
+    this.publicationForm.get('modelName').setValue(event.name);
+  }
+
+  onClearModelDropdown() {
+    this.publicationForm.get('codeModel').setValue(null);
+    this.publicationForm.get('modelName').setValue('');
+  }
+
+  onFilterModel(event: string) {
+    const params = new URLSearchParams();
+    params.set('name_like', event);
+
+    this.loadModel(params);
+  }
+
+  loadModel(params: URLSearchParams) {
+    // cannot load data model when user has selected a product type
+    if (this.publicationForm.get('codeProductType').value) {
+      this.publicationService.getProductModel(params).subscribe(
+        (model: Dropdown[]) => {
+          this.model = model;
+        },
+        error => console.log(error)
+      );
+    }
+  }
+
+  // ===================================================
+  // MANUFACTURER
+  // ===================================================
+  onSelectedItemManufacturer(event) {
+    if (event !== undefined || event !== null) {
+
+      if (event.id) {
+        this.selectManufacturer(event);
+      } else if (event !== '') {
+        // NEW MANUFACTURER
+        const findItem = this.manufacturer.find(
+          p => FormatFieldHelper.removeAccents(p.name).toLowerCase() === FormatFieldHelper.removeAccents(event).toLowerCase());
+        if (findItem) {
+          this.selectManufacturer(findItem);
+          return;
+        }
+
+        console.log('New Manufacturer: ' + event);
+        this.publicationForm.get('codeManufacturer').setValue(null);
+        this.publicationForm.get('manufacturerName').setValue(event);
+      } else {
+        this.onClearModelDropdown();
+      }
+    }
+  }
+
+  selectManufacturer(event: Dropdown) {
+    console.log(event);
+    this.publicationForm.get('codeManufacturer').setValue(event.id);
+    this.publicationForm.get('manufacturerName').setValue(event.name);
+  }
+
+  onClearManufacturerDropdown() {
+    this.publicationForm.get('codeManufacturer').setValue(null);
+    this.publicationForm.get('manufacturerName').setValue('');
+  }
+
+  onFilterManufacturer(event: string) {
+    const params = new URLSearchParams();
+    params.set('name_like', event);
+
+    this.publicationService.getManufacturer(params).subscribe(
+      (manufacturer: Dropdown[]) => {
+        this.manufacturer = manufacturer;
+      },
+      error => console.log(error)
+    );
   }
 
 }
